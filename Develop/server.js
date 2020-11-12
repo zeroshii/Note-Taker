f1d5b4108229c61f538b33c8fc322e19401840f3
Garman_K <garnetbirthstone@hotmail.com>
2020-11-11 20:21:33 -0500

    get and post endpoints

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const uuid = require('uuid');
 const app = express();
+const fs = require('fs');
 
 const PORT = 3000; // process.env.PORT ?
 
@@ -20,7 +21,27 @@ let noteList = [{id: "0000-0000-0000-0000", title: 'note1', text: 'note1 text'}]
 // note: for app.post: newNote.id = uuid.v4() // use a random unique id.
 // ... code away ...
 
+//read the `db.json` file and return all saved notes as JSON.
+app.get('/api/notes', function(req, res){
+    res.json(noteList);
+})
 
+//receive a new note to save on the request
+app.post("/api/notes", function(req, res){
+    const newNote = {
+        id: uuid.v4(),
+        title: req.body.title,
+        text: req.body.text
+    }
+    noteList.push(newNote);
+    fs.appendFileSync(dbFile, newNote);
+    res.send({message: "New note saved."});
+});
+
+// * DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
+app.delete('/api/notes/:id', function (req, res){
+
+})
 // Listener ==================================================
 app.listen(PORT, function() {
     console.log(`Serving notes on PORT ${PORT}`)
